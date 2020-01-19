94342eba9e9b83cf278a78ae5ad5d394c0b60987
samwang <samwang@yipinapp.com>
2020-01-19 15:34:49 +0800

    创建json自动转成object

---
+++ app/src/main/java/com/example/samwang/json2objectdemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.samwang.json2objectdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.gson.Gson;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
